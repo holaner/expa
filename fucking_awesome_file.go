a54f875feffdd1a26e6ebdeef035f4a0a173c41e
holaner <kopernik9999@gmail.com>
2019-11-09 18:27:12 +0500

    very useless

--- fucking_awesome_file.go
+++ fucking_awesome_file.go
@@ -6,4 +6,14 @@ import (
 
 func main() {
     log.Println("i am awesome! no! very awesome!")
+    http.HandleFunc("/", index)
+    http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("за мамку и двор стреляю в упор"))
+}
+
+func usefulFunction(value string) string {
+  return string + string
 }
